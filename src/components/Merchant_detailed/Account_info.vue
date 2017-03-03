1d2a09a64136227b5d8dbcb939acb4075ee58ee9
邵海文 <haiwenshao@gmail.com>
2017-03-03 10:50:20 +0800

    商户管理模板

---
+++ src/components/Merchant_detailed/Account_info.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tab-box">
+        <el-form label-width="130px">
+            <dl class="detailed-box">
+                <dt>
+                    基本信息
+                </dt>
+            </dl>
+        </el-form>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'account',
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
---
+++ src/components/Merchant_detailed/takeaway_info.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tab-box">
+        <el-form label-width="130px">
+            <dl class="detailed-box">
+                <dt>
+                    基本信息
+                </dt>
+            </dl>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'takeaway_info',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
