979363a5f883b894528cca7f0217857c9d23530a
bhm <1291270293@qq.com>
2017-03-03 13:56:54 +0800

    账号信息

--- src/components/Merchant_detailed/Account_info.vue
+++ src/components/Merchant_detailed/Account_info.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="tab-box">
-        <el-form label-width="130px">
+        <el-form label-width="130px" :model="MerchantAccount" :rules="rules" ref="data">
             <dl class="detailed-box">
                 <dt>
                     基本信息
                 </dt>
+                <dd><label class="is-label">商户账号</label> <span>{{MerchantAccount.M_account}}</span></dd>
+                <dd class="required">
+                   <el-form-item label="账号绑定手机号">
+                   <el-input :size="inputSize" v-model="MerchantAccount.M_phone" placeholder="13962250159"></el-input>
+                   </el-form-item>
+                </dd>
+                <dt></dt>
+                 <div class="col-md-8">
+  <span class="wrapper">
+  	<el-button type="info">保存</el-button>
+    <el-button type="warning">重置按钮</el-button>
+  </span>
+</div>      	
             </dl>
         </el-form>
     </div>
@@ -14,10 +27,20 @@
         name: 'account',
         data(){
             return {
-
+               "MerchantAccount":{
+               	"M_account":"ds13962250159",
+               	"M_phone":"13962250159"
+               },
+                inputSize:"small"
             }
         }
     }
 </script>
 <style scoped>
+.col-md-8{
+	position: relative;
+    min-height: 1px;
+   padding-left: 145px;
+}
+
 </style>
