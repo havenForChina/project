50ff9e0866dd00aab4e4e9551878df361986148c
bhm <1291270293@qq.com>
2017-03-03 16:49:21 +0800

    自主营销

---
+++ src/components/Merchant_detailed/Independent.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="tab-box">
+        <el-form label-width="130px">
+            <dl class="detailed-box">           
+                     <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+    <el-tab-pane label="代金券" name="first"></el-tab-pane>
+  </el-tabs>
+  <el-button type="primary">创建进店领代金券</el-button>
+  <el-button type="primary">创建下单返代金券</el-button>
+  <el-button type="primary">创建直领新用户代金券</el-button>             
+            </dl>           
+        </el-form>   
+       <div class="back-box mt-30">
+    		 <el-form :inline="true" :model="Independent" class="demo-form-inline"> 
+    		 	<div class="mt-20 fl">
+    	 <el-form-item label="活动类型">
+              <el-select v-model="Independent.value" placeholder="活动类型" :size="inputSize">
+                <el-option v-for="el in Independent.options"  :label="el.label" :value="el.value" ></el-option>
+              </el-select>
+            </el-form-item>        
+          </div>
+          <div class="mt-20 fl">
+    	 <el-form-item label="活动状态">
+              <el-select v-model="Independent.value" placeholder="活动状态" :size="inputSize">
+                <el-option v-for="el in Independent.options_status"  :label="el.label" :value="el.value" ></el-option>
+              </el-select>
+            </el-form-item>        
+          </div>
+          <div class="mt-20 fl">
+    	 <el-form-item label="城市">
+              <el-select v-model="Independent.value" placeholder="城市" :size="inputSize">
+                <el-option v-for="el in Independent.options_city"  :label="el.label" :value="el.value" ></el-option>
+              </el-select>
+            </el-form-item>        
+          </div>
+          <div class="mt-20 fl">
+    	 <el-form-item label="门店">
+              <el-select v-model="Independent.value" placeholder="门店" :size="inputSize">
+                <el-option v-for="el in Independent.options_shop"  :label="el.label" :value="el.value" ></el-option>
+              </el-select>
+            </el-form-item>        
+          </div>
+                </el-form>
+    	</div>
+             
+    	</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Independent',   
+    data(){
+        return{
+        	 inputSize:'small',
+        	 "Independent":{
+        	 	  "value":1,
+        "options":[
+            {"label":"全部", "value":1},
+            {"label":"进店领券", "value":2},
+            {"label":"下单返券", "value":3},
+            {"label":"新用户券", "value":4}           
+          ],
+          "options_status":[
+            {"label":"全部", "value":1},
+            {"label":"待上线", "value":2},
+            {"label":"上线中", "value":3},
+            {"label":"提前终止", "value":4},
+             {"label":"已结束", "value":4} 
+          ],
+          "options_city":[],
+          "options_shop":[]
+               },
+        activeName2: 'first'
+        }
+        
+    },
+     methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    },
+   
+}
+</script>
+<style scoped>
+.ml-20{
+	margin-left: 40px;
+}
+</style>
--- src/components/Merchant_detailed/Merchant_detailed.vue
+++ src/components/Merchant_detailed/Merchant_detailed.vue
@@ -114,9 +114,7 @@
                 <log></log>
             </el-tab-pane>
             <el-tab-pane label="自主营销" name="5">
-                <div class="tab-box">
-
-                </div>
+                <Independent></Independent>
             </el-tab-pane>
             <el-tab-pane label="合同" name="6">
                 <div class="tab-box">
@@ -135,12 +133,14 @@
     import info from './takeaway_info.vue'
     import Account from './Account_info.vue'
     import log from './operation_log.vue'
+    import Independent from './Independent.vue'
     export default {
         name: 'detailed',
         components:{
             info,
             Account,
-            log
+            log,
+            Independent
         },
         props:{
             data:Object
