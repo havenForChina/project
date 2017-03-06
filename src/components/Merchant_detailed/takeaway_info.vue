0762e6153b5a41c935e42740522fa6b6e2317e04
jingjing994@qq.com <jingjing994@qq.com>
2017-03-06 17:04:26 +0800

    数据绑定

--- src/components/Merchant_detailed/takeaway_info.vue
+++ src/components/Merchant_detailed/takeaway_info.vue
@@ -4,8 +4,8 @@
             <dl class="detailed-box">
                 <dt>营业信息</dt>
                 <dd class="text"><label>商户 Logo</label><span>
-                    <el-radio class="radio" v-model="logo" label="default">默认</el-radio>
-                    <el-radio class="radio" v-model="logo" label="upload">上传</el-radio>
+                    <el-radio class="radio" v-model="business_info.logo" label="default">默认</el-radio>
+                    <el-radio class="radio" v-model="business_info.logo" label="upload">上传</el-radio>
                     <el-upload
                             action="//jsonplaceholder.typicode.com/posts/"
                             list-type="picture-card"
@@ -18,15 +18,15 @@
                     </el-dialog>
                 </span></dd>
                 <dd class="required">
-                    <el-form-item label="外卖客服电话">
-                        <el-input :size="inputSize" placeholder="连锁店标注为“品牌(分店名)形式"></el-input>
+                    <el-form-item label="外卖客服电话" >
+                        <el-input v-model="business_info.phone" placeholder=""></el-input>
                     </el-form-item>
                 </dd>
                 <dd class="required">
                     <el-form-item label="配送时间范围">
                         <el-time-select
                                 placeholder="起始时间"
-                                v-model="startTime"
+                                v-model="business_info.p_startTime"
                                 :picker-options="{
                              start: '08:30',
                              step: '00:15',
@@ -35,12 +35,12 @@
                         </el-time-select>
                         <el-time-select
                                 placeholder="结束时间"
-                                v-model="endTime"
+                                v-model="business_info.p_endTime"
                                 :picker-options="{
                             start: '08:30',
                             step: '00:15',
                             end: '18:30',
-                            minTime: startTime
+                            minTime: business_info.p_startTime
                                              }">
                         </el-time-select>
                     </el-form-item>
@@ -49,7 +49,7 @@
                     <el-form-item label="接单时间范围">
                         <el-time-select
                                 placeholder="起始时间"
-                                v-model="startTime"
+                                v-model="business_info.j_startTime"
                                 :picker-options="{
                              start: '08:30',
                              step: '00:15',
@@ -58,86 +58,86 @@
                         </el-time-select>
                         <el-time-select
                                 placeholder="结束时间"
-                                v-model="endTime"
+                                v-model="business_info.j_endTime"
                                 :picker-options="{
                             start: '08:30',
                             step: '00:15',
                             end: '18:30',
-                            minTime: startTime
+                            minTime: business_info.p_startTime
                                              }">
                         </el-time-select>
                     </el-form-item>
                 </dd>
                 <dd class="text"><label>非营业时间预定</label><span>
-                    <el-radio class="radio" v-model="Reserve" label="Reserve">是</el-radio>
-                    <el-radio class="radio" v-model="Reserve" label="disReserve">否</el-radio>
+                    <el-radio class="radio" v-model="business_info.Reserve" label="Reserve">是</el-radio>
+                    <el-radio class="radio" v-model="business_info.Reserve" label="disReserve">否</el-radio>
                 </span></dd>
             </dl>
             <dl class="detailed-box">
                 <dt>配送相关信息</dt>
-                <dd class="text"><label>配送方</label>
-                <span>
-111
-                </span>
+                <dd class="text" v-model="Distribution"><label>配送方</label><span>{{Distribution.name}}</span>
                 </dd>
                 <dd class="text"><label>需提前预定天数</label><span>
-
-                        <el-input placeholder="请输入内容" v-model="inputSize" style="width:160px ">
+                        <el-input placeholder="请输入内容" v-model="Distribution.supportday" style="width:160px ">
                             <template slot="append">天</template>
                         </el-input>
                     </span>
                 </dd>
                 <dd class="text"><label>支持提前预订天数</label>
-                    <span>1212  </span>
+                    <span><el-input placeholder="请输入内容" v-model="Distribution.reserveday" style="width:160px ">
+                            <template slot="append">天</template>
+                        </el-input>
+                    </span>
                 </dd>
                 <dd class="text"><label>是否支持自动接单</label><span>
-                    <el-radio class="radio" v-model="Autoorder" label="Autoorder">是</el-radio>
-                    <el-radio class="radio" v-model="Autoorder" label="disAutoorder">否</el-radio>
+                    <el-radio class="radio" v-model="Distribution.Autoorder" label="Autoorder">是</el-radio>
+                    <el-radio class="radio" v-model="Distribution.Autoorder" label="disAutoorder">否</el-radio>
                 </span></dd>
                 <dd class="text"><label>配送信息</label>
-                    <span>
-                        <p>配送范围(1)： 配送费3.0元； 配送时长50分钟； 起送价0元; 分级:1级</p>
+                    <span >
+                       <li v-for="(item, index) in Distribution.data">
+                             配送范围[{{ index }}]:配送费{{item.cost}}元;配送时长{{item.time}}分钟;起送价{{item.startprice}}元;分级:{{item.level}}级
+                        </li>
                         <el-button type="text">编辑详情</el-button>
                     </span>
                 </dd>
                 <dd class="text"><label>全城配送</label>
                 <span>
-                    <el-checkbox v-model="allcity">我确定全城配送</el-checkbox>
+                    <el-checkbox v-model="Distribution.allcity">我确定全城配送</el-checkbox>
                 </span>
                 </dd>
             </dl>
-        </el-form>
-        <dl class="detailed-box">
+            <dl class="detailed-box">
             <dt>交易信息</dt>
             <dd class="text"><label>支付方式</label>
                 <span>
-                    <el-checkbox v-model="facetoface">餐到付款</el-checkbox>
-                    <el-checkbox v-model="Online">在线支付</el-checkbox>
+                    <el-checkbox v-model="transaction_info.facetoface">餐到付款</el-checkbox>
+                    <el-checkbox v-model="transaction_info.online">在线支付</el-checkbox>
                     （可多选）
                 </span>
             </dd>
            <dd class="text"><label>支持开发票</label><span>
-                <el-radio class="radio" v-model="invoice" label="invoice">是</el-radio>
-                <el-radio class="radio" v-model="invoice" label="disinvoice">否</el-radio>
+                <el-radio class="radio" v-model="transaction_info.invoice" label="invoice">是</el-radio>
+                <el-radio class="radio" v-model="transaction_info.invoice" label="disinvoice">否</el-radio>
            </span>
            </dd>
             <dd class="text"><label>支持平台代金券</label><span>
-                <el-radio class="radio" v-model="PlatformVouchers" label="Platform">是</el-radio>
-                <el-radio class="radio" v-model="PlatformVouchers" label="disPlatform">否</el-radio>
+                <el-radio class="radio" v-model="transaction_info.PlatformVouchers" label="Platform">是</el-radio>
+                <el-radio class="radio" v-model="transaction_info.PlatformVouchers" label="disPlatform">否</el-radio>
            </span>
             </dd>
             <dd class="text"><label>支持商户代金券</label><span>
-                <el-radio class="radio" v-model="BusinessVouchers" label="Bussiness">是</el-radio>
-                <el-radio class="radio" v-model="BusinessVouchers" label="disBussiness">否</el-radio>
+                <el-radio class="radio" v-model="transaction_info.BusinessVouchers" label="Bussiness">是</el-radio>
+                <el-radio class="radio" v-model="transaction_info.BusinessVouchers" label="disBussiness">否</el-radio>
            </span>
             </dd>
-            <dd class="text"><label> 餐盒单价</label>
-                <span>12213</span>
+            <dd class="text"  v-model="transaction_info"><label> 餐盒单价</label>
+                <span>{{transaction_info.price}}元</span>
             </dd>
             <dd class="text"><label>接单方式</label><span>
-                <el-radio class="radio" v-model="mode" label="1">商户端产品</el-radio>
-                <el-radio class="radio" v-model="mode" label="2">短信接单</el-radio>
-                <el-radio class="radio" v-model="mode" label="3">接单器</el-radio>
+                <el-radio class="radio" v-model="transaction_info.mode" label="1">商户端产品</el-radio>
+                <el-radio class="radio" v-model="transaction_info.mode" label="2">短信接单</el-radio>
+                <el-radio class="radio" v-model="transaction_info.mode" label="3">接单器</el-radio>
            </span>
             </dd>
             <dd class="text"><label> 优惠信息</label>
@@ -146,7 +146,7 @@
                             type="textarea"
                             :rows="2"
                             placeholder="100字以内"
-                            v-model="Discount">
+                            v-model="transaction_info.Discount">
                     </el-input>
                 </span>
             </dd>
@@ -156,7 +156,7 @@
                             type="textarea"
                             :rows="2"
                             placeholder="300字以内"
-                            v-model="Notice">
+                            v-model="transaction_info.Notice">
                     </el-input>
                 </span>
             </dd>
@@ -173,30 +173,43 @@
 <script>
     export default {
         name: 'takeaway_info',
-        props: {
-            data: Object
-        },
         data() {
             return {
-                facetoface:'',
-                Online:'',
-                rules:'',
-                Notice:'',
-                Discount:'',
-                allcity:'',
-                Autoorder:"Autoorder",
-                invoice:'invoice',
-                mode:'1',
-                Reserve:'Reserve',
-                BusinessVouchers:'Business',
-                PlatformVouchers:'Platform',
-                logo: 'default',
                 dialogImageUrl: '',
                 dialogVisible: false,
-                 startTime: '',
-                 endTime: '',
-                inputSize:"small"
+                business_info:{
+                    "logo":"default",
+                    "phone":"13056219558",
+                    "p_startTime": '',
+                    "p_endTime": '',
+                    "j_startTime": '',
+                    "j_endTime": '',
+                    "Reserve":"Reserve"
+                },
+                Distribution: {
+                    "name":"121",
+                    "Autoorder":"disAutoorder",
+                    "allcity":'',
+                    "reserveday":"",
+                    "supportday":"",
+                    "data":[
+                        {"cost":"3","time":"50","startprice":"0","level":"1"},
+                        {"cost":"3","time":"50","startprice":"0","level":"1"}
+                        ]
+                },
+                transaction_info:{
+                    "facetoface":'',
+                    "online":'',
+                    "invoice":'invoice',
+                    "mode":'1',
+                    "PlatformVouchers":'Platform',
+                    "BusinessVouchers":'Business',
+                    "price":'1',
+                    "Notice":'公告',
+                    "Discount":'优惠'
+                }
             };
+
         },
         methods: {
             handleRemove(file, fileList) {
